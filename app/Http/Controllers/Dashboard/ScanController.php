af767c9c33a57fe2f59a3af835eae113dc1d849e
tiarayg <tiarapratiwi889@gmail.com>
2026-01-12 21:21:17 +0700

    fix: scan categories

--- app/Http/Controllers/Dashboard/ScanController.php
+++ app/Http/Controllers/Dashboard/ScanController.php
@@ -12,18 +12,11 @@ class ScanController extends Controller
     {
         $categories = MScanCategory::select('id', 'title', 'icon')->get();
 
-        $data = $categories->map(function ($item) {
-            return [
-                'id' => $item->id,
-                'name' => $item->title, 
-                'icon' => $item->icon,
-            ];
-        });
 
         return response()->json([
             'code' => 200,
             'message' => 'Success',
-            'data' => $data
+            'data' => $categories
         ]);
     }
 }
