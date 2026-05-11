af2c19b7ac05ad6e5ef64d02c49cee6ecb55f292
Stylo AI <stylo.artificial@gmail.com>
2026-05-11 17:45:53 +0700

    fix

--- config/database.php
+++ config/database.php
@@ -96,6 +96,10 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
+
+            'options' => extension_loaded('pdo_pgsql') ? array_filter([
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ]) : [],
         ],
 
         'sqlsrv' => [
